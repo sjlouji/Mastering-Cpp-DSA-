80c6905244ddc4452304c1fab9e06b92eab8b2bb
sjlouji <sjlouji10@gmail.com>
2024-05-27 16:10:06 +0530

    sort people

---
+++ strings/sort_people.cpp
@@ -0,0 +1,53 @@
+//You are given an array of strings names, and an array heights that consists of distinct positive integers. Both arrays are of length n.
+//
+//For each index i, names[i] and heights[i] denote the name and height of the ith person.
+//
+//Return names sorted in descending order by the people's heights.
+//
+//
+//
+//Example 1:
+//
+//Input: names = ["Mary","John","Emma"], heights = [180,165,170]
+//Output: ["Mary","Emma","John"]
+//Explanation: Mary is the tallest, followed by Emma and John.
+//Example 2:
+//
+//Input: names = ["Alice","Bob","Bob"], heights = [155,185,150]
+//Output: ["Bob","Alice","Bob"]
+//Explanation: The first Bob is the tallest, followed by Alice and the second Bob.
+
+
+#include "iostream"
+#include "vector"
+#include <map>
+
+using namespace std;
+
+class Solution {
+public:
+    static vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        vector<pair<int, string>> people;
+
+        for (int i = 0; i < names.size(); ++i) {
+            people.push_back({heights[i], names[i]});
+        }
+        sort(people.begin(), people.end(), [](const pair<int, string>& a, const pair<int, string>& b) {
+            return a.first > b.first;
+        });
+        vector<string> sortedNames;
+        for (const auto& person : people) {
+            sortedNames.push_back(person.second);
+        }
+        return sortedNames;
+    }
+};
+
+int main() {
+    vector<string> vec = {"Mary","John","Emma"};
+    vector<int> heights = {180,165,170};
+    vector<string> s = Solution::sortPeople(vec, heights);
+    for (const auto& res: s) {
+        cout << res << endl;
+    }
+}
