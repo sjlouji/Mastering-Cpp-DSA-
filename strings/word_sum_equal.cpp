72bede10dc18d56a57c1df924745087abcb74f54
sjlouji <sjlouji10@gmail.com>
2024-06-06 10:58:33 +0530

    word sum equal

---
+++ strings/word_sum_equal.cpp
@@ -0,0 +1,16 @@
+#include "iostream"
+
+using namespace std;
+
+class Solution {
+public:
+    bool isSumEqual(string firstWord, string secondWord, string targetWord) {
+        return f(firstWord) + f(secondWord) == f(targetWord);
+    }
+
+    int f(string s) {
+        int res = 0;
+        for (char c : s) res = res * 10 + (c - 'a');
+        return res;
+    }
+};
