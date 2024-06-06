2903f929db5ce606793bd516be0167c1ad7f1f9b
sjlouji <sjlouji10@gmail.com>
2024-06-06 10:39:53 +0530

    are occurrences equal

---
+++ strings/are_all_equal_occurance.cpp
@@ -0,0 +1,24 @@
+#include "iostream";
+
+using namespace std;
+
+class Solution {
+public:
+    bool areOccurrencesEqual(string s) {
+        int cnt[26]{};
+        for (char& c : s) {
+            ++cnt[c - 'a'];
+        }
+        int x = 0;
+        for (int& v : cnt) {
+            if (v) {
+                if (!x) {
+                    x = v;
+                } else if (x != v) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
