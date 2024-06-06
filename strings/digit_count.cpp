259efc65595b4302ab091e20c76d0a8089e44796
sjlouji <sjlouji10@gmail.com>
2024-06-06 21:41:26 +0530

    Digit count

---
+++ strings/digit_count.cpp
@@ -0,0 +1,19 @@
+#include "iostream"
+
+using namespace std;
+
+class Solution {
+public:
+    bool digitCount(string num) {
+        int cnt[10]{};
+        for (char& c : num) {
+            ++cnt[c - '0'];
+        }
+        for (int i = 0; i < num.size(); ++i) {
+            if (cnt[i] != num[i] - '0') {
+                return false;
+            }
+        }
+        return true;
+    }
+};
