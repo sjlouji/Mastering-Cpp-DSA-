d4f264067d0291e7aa6760d30fdce07be378bc53
sjlouji <sjlouji10@gmail.com>
2024-06-06 10:59:32 +0530

    percentage letter

---
+++ strings/percentage_letter.cpp
@@ -0,0 +1,12 @@
+#include "iostream"
+
+using namespace std;
+
+class Solution {
+public:
+    int percentageLetter(string s, char letter) {
+        int cnt = 0;
+        for (char& c : s) cnt += c == letter;
+        return cnt * 100 / s.size();
+    }
+};
