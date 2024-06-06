2216964176507829a0167ee5372c6f79681c7e70
sjlouji <sjlouji10@gmail.com>
2024-06-06 10:34:52 +0530

    remove tailing zeros

---
+++ strings/remove_tailing_zeros.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    string removeTrailingZeros(string num) {
+        while (num.back() == '0') {
+            num.pop_back();
+        }
+        return num;
+    }
+};
