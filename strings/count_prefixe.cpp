14380d1c4965d9cd2b2c1537a58996e5ab2b535d
sjlouji <sjlouji10@gmail.com>
2024-06-06 11:14:16 +0530

    count prefixes

---
+++ strings/count_prefixe.cpp
@@ -0,0 +1,15 @@
+#include "iostream"
+#include "vector"
+
+using namespace std;
+
+class Solution {
+public:
+    int countPrefixes(vector<string>& words, string s) {
+        int ans = 0;
+        for (auto& w : words) {
+            ans += s.starts_with(w);
+        }
+        return ans;
+    }
+};
