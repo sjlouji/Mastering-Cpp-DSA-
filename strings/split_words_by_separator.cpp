5537c01cd7f6ba53034bd4ebebd0b3b931fbe0dc
sjlouji <sjlouji10@gmail.com>
2024-06-06 11:03:05 +0530

    split words by strings

---
+++ strings/split_words_by_separator.cpp
@@ -0,0 +1,21 @@
+#include "iostream"
+#include "vector"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> splitWordsBySeparator(vector<string>& words, char separator) {
+        vector<string> ans;
+        for (const auto& w : words) {
+            istringstream ss(w);
+            string s;
+            while (getline(ss, s, separator)) {
+                if (!s.empty()) {
+                    ans.push_back(s);
+                }
+            }
+        }
+        return ans;
+    }
+};
